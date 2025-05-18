918a9a24938e9f5397ad901fbde46cd373f32637
MurrayEfford <murray.efford@otago.ac.nz>
2025-05-18 17:49:01 +1200

    remove ncores>1

--- R/RFS.R
+++ R/RFS.R
@@ -8,9 +8,7 @@ RFS <- function (randomfn = randomDensity,
 				 detectpar, 
 				 noccasions, 
 				 verbose = FALSE, 
-				 ncores = NULL, 
 				 seed = NULL) {
-	ncores <- setNumThreads(ncores)
 	pd <- pdot(mask, traps, detectfn, detectpar, noccasions)
 	a <- sum(pd) * attr(mask, 'area')
 	En <- a * parm$D
@@ -18,17 +16,8 @@ RFS <- function (randomfn = randomDensity,
 		D <- randomfn(mask, parm)
 		sum(pd * D) / sum(pd) 
 	}
-	if (!is.null(ncores) && ncores>1 && nrepl>1) {
-		clustertype <- if (.Platform$OS.type == "unix") "FORK" else "PSOCK"
-		clust <- parallel::makeCluster(ncores, type = clustertype)
-		parallel::clusterSetRNGStream(clust, seed)
-		on.exit(parallel::stopCluster(clust))
-		localD <- parallel::parSapply(clust, 1:nrepl, one, randomfn, mask, parm, pd)
-	}
-	else {
-		set.seed(seed)
-		localD <- sapply(1:nrepl, one, randomfn, mask, parm, pd)
-	}
+	set.seed(seed)
+	localD <- sapply(1:nrepl, one, randomfn, mask, parm, pd)
 	c <- a * var(localD) / mean(localD) + 1 
 	
 	if (verbose) {
